083b741fe1b360a959586025f1e7fe81d4c3d330
Mridang Agarwalla <mridang@nosto.com>
2021-05-19 23:07:18 +0300

    Cleaned up codecs.

--- jacksandra-core/src/main/java/com/datastax/oss/driver/internal/core/type/codec/extras/time/DurationTypeCodec.java
+++ jacksandra-core/src/main/java/com/datastax/oss/driver/internal/core/type/codec/extras/time/DurationTypeCodec.java
@@ -1,66 +1,29 @@
 package com.datastax.oss.driver.internal.core.type.codec.extras.time;
 
-import java.nio.ByteBuffer;
 import java.time.Duration;
 
-import javax.annotation.Nullable;
-
-import com.datastax.oss.driver.api.core.ProtocolVersion;
-import com.datastax.oss.driver.api.core.type.DataType;
-import com.datastax.oss.driver.api.core.type.DataTypes;
-import com.datastax.oss.driver.api.core.type.codec.TypeCodec;
+import com.datastax.oss.driver.api.core.data.CqlDuration;
+import com.datastax.oss.driver.api.core.type.codec.MappingCodec;
+import com.datastax.oss.driver.api.core.type.codec.TypeCodecs;
 import com.datastax.oss.driver.api.core.type.reflect.GenericType;
 
-import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
 
-public class DurationTypeCodec implements TypeCodec<Duration> {
+public class DurationTypeCodec extends MappingCodec<CqlDuration, Duration> {
 
-    @NonNull
-    @Override
-    public GenericType<Duration> getJavaType() {
-        return GenericType.DURATION;
-    }
-
-    @NonNull
-    @Override
-    public DataType getCqlType() {
-        return DataTypes.DURATION;
-    }
-
-    @Override
-    public boolean accepts(@NonNull Object value) {
-        return value instanceof Duration;
+    public DurationTypeCodec() {
+        super(TypeCodecs.DURATION, GenericType.of(Duration.class));
     }
 
     @Nullable
     @Override
-    public ByteBuffer encode(@Nullable Duration value, @NonNull ProtocolVersion protocolVersion) {
-        return null;
-    }
-
-    @Override
-    public boolean accepts(@NonNull Class<?> javaClass) {
-        return javaClass == Duration.class;
+    protected Duration innerToOuter(@Nullable CqlDuration value) {
+        return value == null ? null : Duration.parse(value.toString());
     }
 
     @Nullable
     @Override
-    public Duration decode(@Nullable ByteBuffer bytes, @NonNull ProtocolVersion protocolVersion) {
-        return null;
-    }
-
-    @NonNull
-    @Override
-    public String format(@Nullable Duration value) {
-        return (value == null) ? "NULL" : value.toString();
-    }
-
-    @Nullable
-    @Override
-    public Duration parse(@Nullable String value) {
-        if (value == null || value.isEmpty() || value.equalsIgnoreCase("NULL")) {
-            return null;
-        }
-        return Duration.parse(value);
+    protected CqlDuration outerToInner(@Nullable Duration value) {
+        return value == null ? null : CqlDuration.from(value.toString());
     }
 }
