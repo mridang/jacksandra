fe696fb6e9a6600fefa49ce0b23b8379bbab3045
Mridang Agarwalla <mridang@nosto.com>
2021-11-01 13:15:11 +0200

    Updated copyrght typos

--- jacksandra-core/src/main/java/com/fasterxml/jackson/datatype/cql/ser/DateAsMillisSerializer.java
+++ jacksandra-core/src/main/java/com/fasterxml/jackson/datatype/cql/ser/DateAsMillisSerializer.java
@@ -1,12 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2021 Nosto Solutions Ltd All Rights Reserved.
- * <p>
- * This software is the confidential and proprietary information of
- * Nosto Solutions Ltd ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the agreement you entered into with
- * Nosto Solutions Ltd.
- ******************************************************************************/
 package com.fasterxml.jackson.datatype.cql.ser;
 
 import java.io.IOException;
